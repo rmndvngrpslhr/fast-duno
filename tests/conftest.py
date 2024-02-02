ab4d591ddf7d1760934095b1d713e8f55aca6d05
Armando Solheiro <avgsolheiro@gmail.com>
2024-02-02 10:33:14 -0300

    Add environment variables to CI pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
 from fast_duno.app import app
 from fast_duno.database import get_session
