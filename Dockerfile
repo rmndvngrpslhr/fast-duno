9f27fb68a2c4229d6addd775268c233ded08c549
Armando Solheiro <avgsolheiro@gmail.com>
2024-02-03 17:03:12 -0300

    Add postgres services at CI

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,4 @@ RUN poetry config installer.max-workers 10
 RUN poetry install --no-interaction --no-ansi
 
 EXPOSE 8000
-<<<<<<< HEAD
 CMD [ "poetry", "run", "uvicorn", "--host", "0.0.0.0", "fast_duno.app:app" ]
-=======
-CMD ["poetry", "run", "uvicorn", "--host", "0.0.0.0", "--port", "8000", "fast_duno.app:app"]
->>>>>>> 4a92694ac858e9c86263e19d6cfdce9ea6b6d813
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 poetry run alembic upgrade head
 
 poetry run uvicorn --host 0.0.0.0 --port 8000 fast_duno.app:app
-=======
-# Executa as migrações do banco de dados
-poetry run alembic upgrade head
-
-# Inicia a aplicação
-poetry run uvicorn --host 0.0.0.0 --port 8000 fast_duno.app:app
->>>>>>> 4a92694ac858e9c86263e19d6cfdce9ea6b6d813
+ 
