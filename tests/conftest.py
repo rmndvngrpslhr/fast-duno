0389a7105d51e3a72b81b48311407928c3803960
Armando Solheiro <avgsolheiro@gmail.com>
2024-02-03 22:06:45 -0300

    Add postgres services at CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -6,19 +5,10 @@ from sqlalchemy.orm import sessionmaker
 
 from fast_duno.app import app
 from fast_duno.database import get_session
-from fast_duno.models import Base, User
+from fast_duno.models import Base
 from fast_duno.security import get_password_hash
 from fast_duno.settings import Settings
-
-
-class UserFactory(factory.Factory):
-    class Meta:
-        model = User
-
-    id = factory.Sequence(lambda n: n)
-    username = factory.LazyAttribute(lambda obj: f'test{obj.id}')
-    email = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
-    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
+from tests.factories import UserFactory
 
 
 @pytest.fixture
